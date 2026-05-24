d8a8bb04c275b265e09b9c4c9a7c3e2daa05248d
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:14:11 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -4,9 +4,10 @@ import type { ValueChangedEvent } from 'devextreme/ui/tag_box';
 import { AppService, type Item } from './app.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   dataSource: DataSource;
